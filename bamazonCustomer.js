88acdbb2131cd15b7e0574c66f4a99e1854de25a
Jake Garlick <jacobgarlick@msn.com>
2019-10-07 18:31:46 -0600

    added prompts and display for current inventory in store

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,9 +9,47 @@ var connection = mySql.createConnection({
     database: "bamazon_db"
 });
 
+function showInventory () {
+    connection.query("SELECT * FROM products", function (err, data) {
+        if (err) throw err;
+        for (var i = 0; i < data.length; i++) {
+            console.log(`\nItem ID: ${data[i].item_id}\nProduct Name: ${data[i].product_name}\nPrice of Product: $${data[i].price}\nAmount in Stock: ${data[i].stock}\nDepartment of Product: ${data[i].department_name}\n--------------------------------------------`)
+        }
+        shopper();
+    })
+};
 
+function shopper() {
+    inquirer.prompt([
+        {
+            type: "number",
+            message: "To proceed with your purchase, please enter the item ID for the product you wish to buy",
+            name: "itemID",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                console.log(`\nMust enter a number for the item ID`)
+                return false;
+            }
+        },
+        {
+            type: "number",
+            message: "Thank you for your selection, please indicate how many units of this product you would like to purchase",
+            name: "itemUnits",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                console.log(`\nMust enter a value for units of product you wish to purchase`)
+                return false;
+            }
+        }
+    ])
+};
 
 connection.connect(function(err){
     if (err) throw err;
     console.log(`Connected as ID ${connection.threadId}`);
+    showInventory();
 });
